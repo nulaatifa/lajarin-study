7d58a537ade871c25f1f0b3981bad29dc5849123
AnahWiyana <Trubusiman2025.example.com>
2025-11-04 23:16:10 -0800

    manambahkan file

--- Anah.py
+++ Anah.py
@@ -3,6 +3,22 @@ Umur= 18 #int
 tinggibadan = 147,8 #float
 perempuan = True #boolean
 nama = "anah" #string
-huruf = "a" #char
+huruf = 'a' #char
 
 # char seharusnya pake tanda petik 1
+
+#soal 2
+# 2. Percabangan
+# buat sebuah variabel bernama umur dengan nilai sembarang terus lakukan kondisi
+# jika umur lebih dari 50 maka print di layar terminal "TUA WOY"
+# sementara jika lebih dari 17 dan dibawah 50 print di layar "dewasa tapi korupsi"
+# dan jika tidak semua itu maka print di layar "aku kecil dan aku masih bahagia"
+# """
+
+umur = 50
+if umur > 50 :
+    print("tua woy")
+elif umur > 17 and umur < 50 :
+    print("dewasa tapi korupsi")
+else :
+    print("aku kecil dan aku masih bahagia")
